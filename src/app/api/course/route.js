5da6bfae10618ae558389c79052391e25cdcb700
Exzq_ <pariwat_wong@cmu.ac.th>
2023-10-02 13:43:51 +0700

    Completed

--- src/app/api/course/route.js
+++ src/app/api/course/route.js
@@ -1,13 +1,21 @@
 import { getPrisma } from "@/libs/getPrisma";
 import { NextResponse } from "next/server";
 
-export const GET = async () => {
+export const GET = async (request) => {
+  const searchText = request.nextUrl.searchParams.get("searchText");
   const prisma = getPrisma();
+
   const courses = await prisma.course.findMany({
+    where: {
+      title: {
+        contains: searchText,
+        mode: "insensitive",
+      },
+    },
     orderBy: {
-      courseNo: "asc",
+      courseNo: "asc"
     },
   });
 
-  return NextResponse.json({ ok: true, courses: courses });
+  return NextResponse.json({ ok: true, courses });
 };
